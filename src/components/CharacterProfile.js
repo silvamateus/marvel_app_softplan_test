0c838440517e51bea09a35d0273eabbd6bbf2840
silvamateus <mateus.wtv@gmail.com>
2018-10-21 11:30:40 -0300

    start character details page

---
+++ src/components/CharacterProfile.js
@@ -0,0 +1,14 @@
+import React from "react"
+import header from "./ProfileHeader"
+
+
+
+const card = props => (
+  <article>
+    {header()}
+    <div>
+      <p>DESCRIPTION</p>
+      <img src="#" alt="imagem"/>
+    </div>
+  </article>
+)

3631e5119cdc099a0b1d70ccbf9603f4b312d9b7
silvamateus <mateus.wtv@gmail.com>
2018-10-21 19:41:54 -0300

    add redux store

---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import { createStore } from "redux"
+import rootReducer from "../reducers/index"
+import { dispatch } from "rxjs/internal/observable/range";
+
+const store = createStore(rootReducer)
+
+export default store

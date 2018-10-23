d772b9f7bbbfd47f230104a59027a8071ff650d6
silvamateus <mateus.wtv@gmail.com>
2018-10-22 23:32:29 -0300

    change Profile Header

--- src/components/ProfileHeader.js
+++ src/components/ProfileHeader.js
@@ -1,19 +1,23 @@
 import React from 'react'
 
-const ProfileImage = props => (
+const ProfileImage = props =>
   <img className="character-image" src={props.imgURL} alt={props.name + " image"}/>
-)
 
-const CharacterName = props => (
-  <h2 className="character-name">{props.name}</h2>
-)
+const CharacterName = props => <h2 className="character-name">{props.name}</h2>
 
+class HeaderCard extends React.Component{
+  render() {
+    const { data } = this.props
+    return (
+      <div className="card" key={data.id}>
+        {data.thumbnail && <ProfileImage 
+          imgURL={`${data.thumbnail.path}.${data.thumbnail.extension}`} 
+          name={data.name}/>}
+        <CharacterName name={data.name} />
+      </div>
+    )
+  }
 
-const headerCard = props => (
-  <div className="card">
-    <ProfileImage imgURL="#" name="PAPAPA"/>
-    <CharacterName name="PAPAPAP" />
-  </div>
-)
+}
 
-export default headerCard
+export default HeaderCard

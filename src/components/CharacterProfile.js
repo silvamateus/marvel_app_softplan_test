21589ea5f994842284def1726a931a826d15e1c1
silvamateus <mateus.wtv@gmail.com>
2018-10-22 23:31:30 -0300

    create CharacterProfile component

--- src/components/CharacterProfile.js
+++ src/components/CharacterProfile.js
@@ -3,12 +3,26 @@ import charHeader from "./ProfileHeader"
 
 
 
-const card = props => (
-  <article>
-    {charHeader()}
-    <div>
-      <p>DESCRIPTION</p>
-      <img src="#" alt="imagem"/>
+export default class Card extends React.Component {
+
+  render() {
+    const data = this.props.data || []
+    const charData = data.find((element, index )=> Number(element.id) == Number(this.props.match.params.id) )
+    return (
+    <article className="card-block">
+      <CharHeader data={charData}/>
+      <div className="card-body">
+        <p>{charData.description}</p>
+        <div className="infos">
+          <h2>Series</h2>
+          {charData.series.items.map(
+            item => (
+              <h3>
+                {item.name}
+              </h3>
+            ))
+          }
+        </div>      
     </div>
   </article>
 )

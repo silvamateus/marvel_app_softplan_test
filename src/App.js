2a9249c56c332374638200ad7527a397cf973579
silvamateus <mateus.wtv@gmail.com>
2018-10-22 23:28:24 -0300

    add Router Basis

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import marvelHeader from "./components/Header"
 import Card from "./components/ProfileHeader"
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link} from "react-router-dom"
+import CharDetail from './components/CharacterProfile'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {marvelHeader()}
-        {Card()}
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/details/:id" component={Detail}/>
+          <Route path="/List" component={List}/>
+        </Switch>
+    </Router>
       </div>
     );
   }
